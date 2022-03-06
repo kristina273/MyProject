a93e994f16f016a702ef98f9e0cb9a6082ad531d
Kristina273 <opacakkristina1@gmail.com>
2022-03-06 15:30:52 +0100

    Dodane nove funkcije

--- file1.py
+++ file1.py
@@ -1,1 +1,39 @@
-print("Hello world.")
+import matplotlib.pyplot as plt
+import numpy as np
+
+# while True:
+#     x1, y1 = input("Unesite koordinate točke A: ").split()
+#     x2, y2 = input("Unesite koordinate točke B: ").split()
+#     try:
+#         x1 = int(x1)
+#         y1 = int(y1)
+#         x2 = int(x2)
+#         y2 = int(y2)
+#         break;
+#     except:
+#         print("Molim unesite numeričku vrijednost.")
+
+# a=(y2-y1)/(x2-x1)
+# b=x2-x1
+# print(" y -",y1,"=",a,"( x -",x1,")")
+
+def fun(x1,y1,x2,y2):
+    a=(y2-y1)/(x2-x1)
+    b=x2-x1
+    print(" y -",y1,"=",a,"( x -",x1,")")
+
+    xaxis = np.array([2,8])
+    yaxis = np.array([4,9])
+
+    plt.plot(xaxis,yaxis)
+    x = input("Ako želite prikazati graf, unesite 1. \nAko želite spremiti kao PDF, unesite 2. ")
+    if x=='1':
+        plt.show()
+    elif x=='2':
+        name = input("Unesite željeno ime datoteke: ")
+        plt.savefig(name)  
+
+
+
+fun(1,2,3,4)
+
